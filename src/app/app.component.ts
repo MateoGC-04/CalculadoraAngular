b1e50ee5e9e041327f88f2d8b6fe37d711e513a2
MateoGC-04 <mateogonzalezcano04@gmail.com>
2023-05-31 01:48:42 -0500

    buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,58 +6,158 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-angular';
+  title = 'proyect';
 }
 
-let num1
-let num2
-let respt
-let resptString 
-let resptContainer = <HTMLElement>document.getElementById('respuesta');
-const button1 = document.getElementById('suma')
-const button2 = document.getElementById('resta')
-const button3 = document.getElementById('mult')
-const button4 = document.getElementById('div')
+const button0 = document.getElementById('Zero');
+const button1 = document.getElementById('One');
+const button2 = document.getElementById('Two');
+const button3 = document.getElementById('Three');
+const button4 = document.getElementById('Fourth');
+const button5 = document.getElementById('Five');
+const button6 = document.getElementById('Six');
+const button7 = document.getElementById('Seven');
+const button8 = document.getElementById('Eight');
+const button9 = document.getElementById('Nine');
 
-button1?.addEventListener("click",function handleclick(event)
-{
-num1 = parseFloat((<HTMLInputElement>document.getElementById('num1')).value);
-num2 = parseFloat((<HTMLInputElement>document.getElementById('num2')).value);
-respt = (num1 + num2);
-resptString = respt.toString();
-resptContainer.innerHTML = resptString;
+const buttonSum = document.getElementById('Sum');
+const buttonRest = document.getElementById('Rest');
+const buttonMult = document.getElementById('Mult');
+const buttonDiv = document.getElementById('Div');
+
+const buttonDEL = document.getElementById('DEL');
+const buttonAC = document.getElementById('AC');
+const buttonAns = document.getElementById('Ans');
+
+const buttonSin = document.getElementById('Sin');
+const buttonCos = document.getElementById('Cos');
+const buttonTan = document.getElementById('Tan');
+
+const buttonIgual = document.getElementById('Igual');
+const buttonPotencia = document.getElementById('Potencia');
+const buttonRaiz = document.getElementById('Raiz');
+const buttonPi = document.getElementById('Pi');
+const buttonP = document.getElementById('Point');
+let contenedor
+
+button1?.addEventListener('click', function handleClick(event) {
+
+    let myContainer = <HTMLElement>document.getElementById('tabla');
+    contenedor = myContainer.innerHTML + "1"
+    myContainer.innerHTML = contenedor;
+  
+});
+button2?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "2"
+  myContainer.innerHTML = contenedor;
+
+});
+button3?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "3"
+  myContainer.innerHTML = contenedor;
+
+});
+button4?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "4"
+  myContainer.innerHTML = contenedor;
+
+});
+button5?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "5"
+  myContainer.innerHTML = contenedor;
+
+});
+button6?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "6"
+  myContainer.innerHTML = contenedor;
+
+});
+button7?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "7"
+  myContainer.innerHTML = contenedor;
+
+});
+button8?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "8"
+  myContainer.innerHTML = contenedor;
+
+});
+button9?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "9"
+  myContainer.innerHTML = contenedor;
+
+});
+button0?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "0"
+  myContainer.innerHTML = contenedor;
+
+});
+buttonP?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + "."
+  myContainer.innerHTML = contenedor;
+
 });
 
-button2?.addEventListener("click",function handleclick(event)
-{
-num1 = parseFloat((<HTMLInputElement>document.getElementById('num1')).value);
-num2 = parseFloat((<HTMLInputElement>document.getElementById('num2')).value);
-respt = (num1 - num2);
-resptString = respt.toString();
-resptContainer.innerHTML = resptString;
+buttonSum?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + " + "
+  myContainer.innerHTML = contenedor;
+
+});
+buttonRest?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + " - "
+  myContainer.innerHTML = contenedor;
+
+});
+buttonMult?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + " * "
+  myContainer.innerHTML = contenedor;
+
 });
 
-button3?.addEventListener("click",function handleclick(event)
-{
-num1 = parseFloat((<HTMLInputElement>document.getElementById('num1')).value);
-num2 = parseFloat((<HTMLInputElement>document.getElementById('num2')).value);
-respt = (num1 * num2);
-resptString = respt.toString();
-resptContainer.innerHTML = resptString;
+buttonDiv?.addEventListener('click', function handleClick(event) {
+
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  contenedor = myContainer.innerHTML + " / "
+  myContainer.innerHTML = contenedor;
+
 });
 
-button4?.addEventListener("click",function handleclick(event)
-{
-num1 = parseFloat((<HTMLInputElement>document.getElementById('num1')).value);
-num2 = parseFloat((<HTMLInputElement>document.getElementById('num2')).value);
-if (num2 == 0) 
-{
-  resptString = "Math error";
-} 
-else 
-{
-  respt = (num1 / num2);
-  resptString = respt.toString();
-}
-resptContainer.innerHTML = resptString;
+buttonIgual?.addEventListener('click', function handleClick(event) {
+  let operacion
+  let operacion2
+  
+  operacion = (<HTMLElement><unknown>document.getElementById('tabla')?.innerHTML)
+  operacion2 = operacion.toString()
+  operacion = eval(operacion2)
+  operacion2 = operacion.toString()
+  
+  let myContainer = <HTMLElement>document.getElementById('tabla');
+  myContainer.innerHTML = operacion2;
+
 });
